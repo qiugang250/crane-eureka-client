9060ee875442492d874348406807b8efbfbe0ec5
qiugang <849888900@qq.com>
2018-09-19 11:22:33 +0800

    create

---
+++ src/main/java/mavenproject/eureka_client/EurekaClientApplication.java
@@ -0,0 +1,26 @@
+package mavenproject.eureka_client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+//未加RestController未报错，也不出结果
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run( EurekaClientApplication.class, args );
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "crane") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+}
